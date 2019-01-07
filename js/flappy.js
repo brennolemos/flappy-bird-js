697fa30683c2996c666893ed197773b589150b25
Breno Lemos <brenomiros@gmail.com>
2019-01-06 21:39:22 -0300

    Funcionalidade de Checagem de Colisão

--- js/flappy.js
+++ js/flappy.js
@@ -6,6 +6,7 @@ function novoElemento(tagName, className) {
 
 function Barreira(reversa = false) {
     this.elemento = novoElemento('div', 'barreira');
+
     const borda = novoElemento('div', 'borda');
     const corpo = novoElemento('div', 'corpo');
 
@@ -31,7 +32,7 @@ function ParDeBarreiras(altura, abertura, x) {
     }
 
     this.getX = () => parseInt(this.elemento.style.left.split('px')[0]);
-    this.setX = () => this.elemento.style.left = `${x}px`;
+    this.setX = x => this.elemento.style.left = `${x}px`;
     this.getLargura = () => this.elemento.clientWidth;
 
     this.sortearAbertura();
@@ -43,13 +44,13 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
         new ParDeBarreiras(altura, abertura, largura),
         new ParDeBarreiras(altura, abertura, largura + espaco),
         new ParDeBarreiras(altura, abertura, largura + espaco * 2),
-        new ParDeBarreiras(altura, abertura, largura + espaco * 3),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3)
     ]
 
     const deslocamento = 3;
     this.animar = () => {
         this.pares.forEach(par => {
-            par.setX(par.getX - deslocamento);
+            par.setX(par.getX() - deslocamento);
 
             // Quando o elemento sair da área do jogo
             if (par.getX() < -par.getLargura()) {
@@ -93,16 +94,6 @@ function Passaro(alturaJogo) {
 }
 
 
-// const barreiras = new Barreiras(700, 1200, 200, 400)
-// const passaro = new Passaro(700);
-// const areaDoJogo = document.querySelector('[wm-flappy]')
-
-// areaDoJogo.appendChild(passaro.elemento)
-// barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
-// setInterval(() => {
-//     barreiras.animar()
-//     passaro.animar()
-// }, 20)
 
 function Progresso() {
     this.elemento = novoElemento('span', 'progresso');
@@ -110,6 +101,32 @@ function Progresso() {
         this.elemento.innerHTML = pontos
     }
     this.atualizarPontos(0);
+}
+
+function estaoSobrePostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect();
+    const b = elementoB.getBoundingClientRect();
+
+    const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left;
+    const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top;
+
+    return horizontal && vertical;
+
+}
+
+function colidiu(passaro, barreiras) {
+    let colidiu = false;
+
+    barreiras.pares.forEach(parDeBarreiras => {
+        if (!colidiu) {
+            const superior = parDeBarreiras.superior.elemento;
+            const inferior = parDeBarreiras.inferior.elemento;
+
+            colidiu = estaoSobrePostos(passaro.elemento, superior) || estaoSobrePostos(passaro.elemento, inferior);
+        }
+    });
+
+    return colidiu;
 }
 
 function FlappyBird() {
@@ -130,6 +147,10 @@ function FlappyBird() {
         const temporizador = setInterval(() => {
             barreiras.animar();
             passaro.animar();
+
+            if (colidiu(passaro, barreiras)) {
+                clearInterval(temporizador);
+            }
         }, 20);
     }
 
